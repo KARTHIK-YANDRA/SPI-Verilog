b436addc9b295684e15392b08e8ee2f5cf0088e1
KARTHIK-YANDRA <karthikyandra7777@gmail.com>
2026-05-14 12:28:58 +0530

    Update spi_slave.v

--- spi_slave.v
+++ spi_slave.v
@@ -4,11 +4,14 @@ module spi_slave(
     input reset,
     input ss,
     input mosi,
-    output reg[7:0] data_out
+	 input [7:0] data_in,
+    output reg[7:0] data_out,
+	 output reg miso
     );
 	 
 	 reg [7:0] shift_reg;
 	 reg [2:0] bit_count;
+	 reg [7:0] tx_shift_reg;
 	 
 	 always @(posedge sclk or posedge reset)
 	 begin
@@ -17,18 +20,30 @@ module spi_slave(
 		 shift_reg <= 0;
 		 bit_count <= 0;
 		 data_out <= 0;
+		 tx_shift_reg <= 0;
+		 miso <= 0;
 		end
 		
 	else
 	begin 
 	 if(ss==0)
 	 begin
+	  if(bit_count == 0)
+	  begin
+	  miso <= data_in[7];
+	  tx_shift_reg <= data_in << 1;
+	  end
+	  else
+	  begin
+	   miso <= tx_shift_reg[7];
+		tx_shift_reg <= tx_shift_reg << 1;
+		end
 	   shift_reg <= {shift_reg[6:0],mosi};
 		bit_count <= bit_count+1;
 		if(bit_count == 7)
 		begin
 		  data_out <= {shift_reg[6:0],mosi};
-		  bit_count = 0;
+		  bit_count <= 0;
 		end
     end
 	end
